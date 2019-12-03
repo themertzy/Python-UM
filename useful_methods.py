8f78ba143d603af0656dad4ea3e4f829a9374fa7
themertzy <58080566+themertzy@users.noreply.github.com>
2019-12-03 09:45:11 -0600

    iteration Fibonacci sequence added

--- useful_methods.py
+++ useful_methods.py
@@ -561,3 +561,17 @@ def fibb_recur(n):
 		return(fibb_recur(n-1) + fibb_recur(n-2))
 
 ##################################################################################################################################################################################
+
+#A Fibonacci Sequence Function using iteration.  Iteration is often considered to be slower than a recursive approach.
+
+def fib_iteration(n):
+    
+	a = 0
+	b = 1
+    
+	for i in range(0, n):
+        	temp = a
+        	a = b
+        	b = temp + b
+		
+    	return a
