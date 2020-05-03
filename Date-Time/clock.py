102dd76abd3dd696652a56c03b4417380b1e815b
Isaac Mertzenich <isaac.mertzenich@gmail.com>
2020-05-03 14:02:00 -0500

    clock added

--- Date-Time/clock.py
+++ Date-Time/clock.py
@@ -1,3 +1,29 @@
 #
 #An implementation of a clock with full features.
 #
+
+
+class PyClock:
+
+    def __init__(self):
+
+        return None
+
+    def currentTime(self):
+
+        return None
+
+    def currentDate(self):
+
+        return None
+
+    def currentMonth(self):
+
+        return None
+
+    def currentDay(self):
+
+        return None
+
+
+    
