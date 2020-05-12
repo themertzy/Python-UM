687cab2205713edff4b75b0418b5c487fe25b098
themertzy <58080566+themertzy@users.noreply.github.com>
2020-05-12 14:46:40 -0500

    comment changes

--- Files/ftl.py
+++ Files/ftl.py
@@ -1,16 +1,11 @@
 #This function can be used to read in a file and split it along a certain
-
 #designated delimeter. I have found this particularly usefull when dealing
-
 #with lists of items in a file that require the replacement of a common delimeter.
 
-#Note: This program returns a list of the items split at the given delimeter. If this
-
-#delimeter is a new line character or something of that sort, then the delimeter
-
-#needs to be put in with quotes. ALWAYS PUT FILE NAMES IN QUOTES!!!
-
-#This function returns False if it fails to open, read, or split the text.
+###Note: This program returns a list of the items split at the given delimeter. If this
+###delimeter is a new line character or something of that sort, then the delimeter
+###needs to be put in with quotes. ALWAYS PUT FILE NAMES IN QUOTES!!!
+###This function returns False if it fails to open, read, or split the text.
 
 
 def ftl(inputFile, delimeter):
